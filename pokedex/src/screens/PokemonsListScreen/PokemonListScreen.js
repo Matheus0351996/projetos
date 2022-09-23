cb6ab8f893d2abec6bc15d26b4bf683e10d3482a
matheus <mnatalsantos@hotmail.com>
2022-09-23 02:17:39 -0300

    alterações no header,consertar o voltar

--- pokedex/src/screens/PokemonsListScreen/PokemonListScreen.js
+++ pokedex/src/screens/PokemonsListScreen/PokemonListScreen.js
@@ -3,12 +3,17 @@ import PokemonCard from "../../components/PokemonCard/PokemonCard";
 import Header from "../../components/Header/Header";
 import { PokeListContainer } from "./styled";
 import GlobalStateContext from "../../global/GlobalStateContext";
+import { goToPokedex } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
 
 const PokemonListScreen = () => {
     const {pokemons} = useContext(GlobalStateContext)
+    const navigate = useNavigate
     return (
     <>
-        <Header/>
+        <Header title = {"Lista de Pokemons"}
+        leftButtonFunction = {()=>goToPokedex(navigate)}
+        />
         <PokeListContainer>
             {
                 pokemons.map((poke) => {
