2279913bb116d30eeacef4592e97552e948ca090
matheus <mnatalsantos@hotmail.com>
2022-10-01 15:33:58 -0300

    Criação do projeto

---
+++ typescript-projects/galery/src/App.tsx
@@ -0,0 +1,18 @@
+import * as  C from './App.styles'
+
+const App = () => {
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de fotos</C.Header>
+
+        {/* Area de upload */}
+
+        
+        {/* Lista de fotos */}
+      </C.Area>
+    </C.Container>
+  )
+}
+
+export default App;
